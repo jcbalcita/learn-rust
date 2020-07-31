134571cd9288dbc1616682c62fba60c82d8f9016
JC Balcita <jc.balcita@hulu.com>
2020-07-31 11:51:51 -0700

    variable types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    // pattern matching assignment
+    let (mut bunnies, carrots) = (8, 50);
+    bunnies = 3;
+    // carrots = 5   <= causes error
+
+    // constants (must be definable at COMPILE time)
+    const THIS_CONSTANT: &str = "that";
+
+    hello_world();
 }
+
+fn hello_world() -> &str {
+    const HELLO_WORLD: &str = "Hello, world!";
+    println!(HELLO_WORLD);
+    HELLO_WORLD
+}
+
